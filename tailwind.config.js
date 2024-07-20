7f73e56384f3f93204c2d06f30dfbd43d704e59d
Ali Hamrani <ali.hamrani2001@gmail.com>
2024-07-20 12:30:12 +0330

    feat: landing page completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './layouts/**/*.html',
@@ -15,6 +17,9 @@ module.exports = {
         'top-left-to-bottom-right': '1px 1px 1px 1px rgba(63,65,83, 0.5), 1px 1px 1px 1px rgba(140,144,185, 1)',
         'single-footer-shadow': '0px -30px 20px rgba(132, 132, 132, 0.11)',
         "get-pac": "0px 0px 40px rgba(89, 64, 246, 0.3)"
+      },
+      fontFamily: {
+        'sans': ['"Inter"','sans-serif', ...defaultTheme.fontFamily.sans],
       },
     },
     colors: {
