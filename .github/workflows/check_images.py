9097189c3f56986a448b645386548536141eaba9
Mostafa <mostafa.sedaghat@gmail.com>
2024-10-21 14:15:40 +0800

    update github action for linting project

--- .github/workflows/check_images.py
+++ .github/workflows/check_images.py
@@ -4,13 +4,14 @@ import sys
 def check_image_file_names(root_dir):
     for root, _, files in os.walk(root_dir):
         for file in files:
-            if file.lower().endswith(('.png', '.gif')):
+            if file.lower().endswith(('.jpg', '.png', '.gif')):
                 if '_' in file:
                     print(f"Image file {os.path.join(root, file)} should bot have '_' in  its path.")
                     sys.exit(1)
 
-            elif file.lower().endswith(('.jpg', '.jpeg', '.bmp', '.tiff', '.webp')):
-                print(f"Image file {os.path.join(root, file)} is not a PNG or GIF and is not checked.")
+            if file.lower().endswith(('.jpeg', '.bmp', '.tiff', '.webp')):
+                print(f"Image file {os.path.join(root, file)} is not a JPG, PNG or GIF.")
+                sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
