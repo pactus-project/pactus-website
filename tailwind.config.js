0c7d7d857a7b07e1132c2611a3c4370061233fbd
b00f <mostafa.sedaghat@gmail.com>
2025-11-08 19:36:48 +0800

    refactor layouts, upgrade Hugo (#213)

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,6 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "landing-top-img": "url('/images/landing-top-bg.svg')",
-        "empower-trust": "url('/images/bg-empowering-trust.svg')",
       },
       boxShadow: {
         'top-left-to-bottom-right': '1px 1px 1px 1px rgba(63,65,83, 0.5), 1px 1px 1px 1px rgba(140,144,185, 1)',
